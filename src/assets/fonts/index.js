a5b759a824b20756a7994261a9fd3da83274cad4
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2020-09-24 10:42:06 +0000

    feat: add assets

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,13 @@
+import BLOKKNeueRegularEOT from "./BLOKKNeue-Regular.eot";
+import BLOKKNeueRegularOTF from "./BLOKKNeue-Regular.otf";
+import BLOKKNeueRegularTTF from "./BLOKKNeue-Regular.ttf";
+import BLOKKNeueRegularWOFF from "./BLOKKNeue-Regular.woff";
+import BLOKKNeueRegularWOFF2 from "./BLOKKNeue-Regular.woff2";
+
+export default {
+  BLOKKNeueRegularEOT,
+  BLOKKNeueRegularOTF,
+  BLOKKNeueRegularTTF,
+  BLOKKNeueRegularWOFF,
+  BLOKKNeueRegularWOFF2,
+};
