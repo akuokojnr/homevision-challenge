fc76d87a7f7caa6ba0b0eb8fc286eb99cc4fce09
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2020-09-24 11:54:26 +0000

    fix: font-display

--- src/components/common/GlobalStyles.js
+++ src/components/common/GlobalStyles.js
@@ -5,11 +5,12 @@ import fonts from "../../assets/fonts";
 const GlobalStyles = createGlobalStyle`
     @font-face {
     font-family: "Blokk";
-    src: url(${fonts.BLOKKNeueRegularEOT});
+    font-display: auto;
     src: url(${fonts.BLOKKNeueRegularWOFF2}) format("woff2"),
          url(${fonts.BLOKKNeueRegularWOFF}) format("woff"),
          url(${fonts.BLOKKNeueRegularTTF}) format("ttf"),
-         url(${fonts.BLOKKNeueRegularOTF}) format("otf");
+         url(${fonts.BLOKKNeueRegularOTF}) format("otf"),
+         url(${fonts.BLOKKNeueRegularEOT});
 }
 `;
 
